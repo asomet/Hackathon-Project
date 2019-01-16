88681da0fb143a102b34a45d0ee3f0ee8439fc2a
Alain Somet <sometalain@yahoo.fr>
2019-01-16 12:02:27 +0100

    first commit

---
+++ app/src/main/java/fr/wildcodeschool/airbusproject/MainActivity.java
@@ -0,0 +1,13 @@
+tatuspackage fr.wildcodeschool.airbusproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
